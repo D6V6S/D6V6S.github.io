cc48d43f33cc7c871ff4327534095b02dc39ddfc
D6V6S <96797621+D6V6S@users.noreply.github.com>
2022-07-04 22:15:30 +0300

    8-th lesson

--- js/script.js
+++ js/script.js
@@ -49,7 +49,7 @@ icons.forEach (icon => {
 // function navBarShow() {
 //   var x = document.getElementById("navBarMobile");
 //     x. classList.toggle("show");
-// } 
+// }
 
 
 
@@ -61,7 +61,7 @@ icons.forEach (icon => {
 //   } else {
 //     x.classList = "navbar-collapse collapse show";
 //   }
-// } 
+// }
 
 
 /* Toggle  */
@@ -72,7 +72,7 @@ icons.forEach (icon => {
 //   } else {
 //     x.style.display = "block";
 //   }
-// } 
+// }
 
 /* Toggle  */
 // function navBarShowV2() {
@@ -106,4 +106,40 @@ icons.forEach (icon => {
 
 
 //   }
-// } 
+// }
+
+
+window.addEventListener("DOMContentLoaded",() => {
+	const btn = document.querySelector("button");
+	var doneTimeout = null,
+		resetTimeout = null;
+
+	if (btn) {
+		btn.addEventListener("click",function() {
+			const runClass = "btn--running";
+			const doneClass = "btn--done";
+			// `.btn--running .btn__progress-fill` `stroke-dashoffset` duration in ms
+			const submitDuration = 2000;
+			const resetDuration = 1500;
+
+			// fake the submission
+			this.disabled = true;
+			this.classList.add(runClass);
+
+			clearTimeout(doneTimeout);
+			clearTimeout(resetTimeout);
+
+			doneTimeout = setTimeout(() => {
+				this.classList.remove(runClass);
+				this.classList.add(doneClass);
+				
+				// reset the button
+				resetTimeout = setTimeout(() => {
+					this.disabled = false;
+					this.classList.remove(doneClass);
+				}, resetDuration);
+
+			}, 600 + submitDuration);
+		});
+	}
+});
